f087b8869c3eb3265117240b9ea2ede144ee24d2
ibrahimmenem <ibrahimmun@hotmail.com>
2012-05-31 20:15:16 +0200

    tagvideo working, GB_1.csv  and  GS_1.cvs done

--- tagvideo.py
+++ tagvideo.py
@@ -2,7 +2,7 @@
 
 # example checkbutton.py
 
-import pygtk ,sys , cv2
+import pygtk ,sys , cv2 ,os,numpy
 pygtk.require('2.0')
 import gtk
 
@@ -10,7 +10,7 @@ class VideoTagger:
     # Our callback.
     # The data passed to this method is printed to stdout
     def callback(self, widget,data=None):
-              print widget.get_name() 
+              #print widget.get_name() 
               if widget.get_active():
                     for item in widget.get_parent():
                          item.set_active(True)
@@ -19,45 +19,49 @@ class VideoTagger:
                          item.set_active(False)
 
     # This callback quits the program
-    def delete_event(self, widget, event, data=None):
+    def delete_event(self,*args):
         video_capture.release()
         video_tags_file.close()
         gtk.main_quit()
         return False
     
     def nextframe(self, *args):
-        line=''
-        for item in self.record_holder:
-              line=line+str(item)+','
-        video_tags_file.write(line+'\n')
-        print "Saving: "+line+'\n'
-         
-        self.record_holder=[] # empty record holder
+         if not self.finished:
+             for i in range(FPS/DSF_rate):
+                       video_capture.grab()
+                       self.Read_Frames=self.Read_Frames+1
+             if   self.Read_Frames>=FRAME_COUNT:
+                  print "All frames have been read!"  
+                  self.finished=1	
+  
+             frame=cv2.pyrDown(video_capture.retrieve()[1])  # spacial downscale 
+             image.set_from_pixbuf(gtk.gdk.pixbuf_new_from_array(numpy.array(frame), gtk.gdk.COLORSPACE_RGB, 8))
 
-        for i in range(FPS/DSF_rate):
-             video_capture.grab()
-             self.Read_Frames=self.Read_Frames+1
-             if self.Read_Frames>=FRAME_COUNT:
-                 print "All frames have been read!"   
-                 return
-        #print "Processing frame number {0} ".format(Read_Frames)
-        _,frame=video_capture.retrieve()
+             t= float(self.Read_Frames)/FPS 
+     
+     
+             self.record_holder.append(t)
+             for chkbox in self.chkboxs:
+                   if chkbox.get_active()==True:
+                       self.record_holder.append(chkbox.get_name())
 
-        t= float(self.Read_Frames)/FPS 
+             if not self.finished:
+                 line=''
+                 for item in self.record_holder:
+                       line=line+str(item)+','
+                 video_tags_file.write(line.strip(',')+'\n')
+                 print "Saving: "+line.strip(',')+'\n'
+                  
+                 self.record_holder=[] # empty record holder        
+                      
 
-        cv2.imshow("frame",frame)
-        self.record_holder.append(t)
-        for chkbox in self.chkboxs:
-              if chkbox.get_active()==True:
-                  self.record_holder.append(chkbox.get_name())
-         
     def __init__(self):
-        # Create a new window
+        self.finished=0
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("tag video")
         self.window.connect("delete_event", self.delete_event)
         self.window.set_border_width(20)
-        hbox = gtk.HBox(True, 2)
+        hbox = gtk.HBox(False, 2)
         self.window.add(hbox)
         logolist={ 'GB':['Logo_FCB','Logo_pepsi','Logo_LibraryAdvocacy','Logo_BMW','Logo_ferrari','Logo_facebook','Logo_linux','Logo_chrome','Logo_apple','Logo_DOMINOSPIZZA','Logo_fifa','Logo_Firefox'],
                         'GM':['Logo_BurgerKing','Logo_GENERALI','Logo_Lexus','Logo_Mcdonalds','Logo_Mercedes','Logo_intel','Logo_mazda','Logo_msn','logo-iberia','logo_indra','Logo_carrefour','Logo_mastercard'],
@@ -65,7 +69,7 @@ class VideoTagger:
                         'GS':['Logo_SEAT','Logo_Skype','Logo_Sony_Ericsson','Logo_Starbucks','Logo_TELEFONICA','Logo_UPM','Logo_Wikipedia','Logo_Windows','Logo_samsung','Logo_toyota','Logo_twitter','Logo_visa','Logo_xeon','logo_santander']}
         self.chkboxs=[]
         for videogroup, logogroup in logolist.items():
-             vbox= gtk.VBox(True, 2)
+             vbox= gtk.VBox(False, 2)
              button=gtk.CheckButton(videogroup)
              button.connect("toggled", self.callback, videogroup) 
              button.set_name(videogroup)
@@ -90,7 +94,7 @@ class VideoTagger:
         vbox.pack_start(button, True, True, 2)
 
         hbox.pack_start(vbox, True, True, 2)
-
+        hbox.pack_start(image, True, True, 2)
         self.Read_Frames=0
         self.record_holder=[] 
         self.window.show_all()
@@ -124,16 +128,24 @@ if __name__ == "__main__":
              DSF_rate=int( sys.argv[2] )  # frequency downscale rate
         else:
              print "bad input arguments!"
+             print """Usage:  
+                                     tagvideo.py [videopath] [framerate]  """
              sys.exit()
 
-        video_tags_file=open("./Videos/Tags_"+video_fn.split('.')[0]+".csv",'w')
+        dir,file= os.path.split(video_fn)
+        video_tags_file=open(dir+"/"+file.split('.')[0]+".csv",'w')
         video_capture=cv2.VideoCapture(video_fn)
         video_capture.open(video_fn) 
-        
-        _,frame=video_capture.read()
+
+        frame=cv2.pyrDown(video_capture.read()[1])  # spacial downscale 
+
+        #cv2.imshow("frame",frame)
+        image = gtk.Image()
+
         props= ReadVideoProps(video_capture)
         FPS=int(props['FPS'])
         FRAME_COUNT=int(props['FRAME_COUNT'])
-        
+        image.set_from_pixbuf(gtk.gdk.pixbuf_new_from_array(numpy.array(frame), gtk.gdk.COLORSPACE_RGB, 8))
+
         VideoTagger()
         main()
